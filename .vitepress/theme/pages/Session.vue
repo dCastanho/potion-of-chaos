acd724c9d415154c5bd984b1b669253567182709
dCastanho <dc@cob.pt>
2025-01-31 23:09:57 +0000

    Fixed mobile margins

--- .vitepress/theme/pages/Session.vue
+++ .vitepress/theme/pages/Session.vue
@@ -7,7 +7,7 @@ const { frontmatter } = useData()
 </script>
 
 <template>
-	<div class="px-12 py-12 grow overflow-y-scroll lg:px-8">
+	<div class="px-6 py-12 grow overflow-y-scroll lg:px-8">
 	  <div class="mx-auto max-w-3xl text-base/7 text-slate-200 text-justify">
 		<p class="text-base/7 font-semibold text-[#b89e44]">{{frontmatter.date}}</p>
 		<h1 class="mt-2 mb-8 text-5xl font-semibold text-left text-balance text-gray-100 ">Session {{frontmatter.number}}: {{ frontmatter.title }} <span class=" text-[#b89e44] text-sm">Arc {{ frontmatter.arc }}</span></h1>
